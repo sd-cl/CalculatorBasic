f1b9f9804dd256f0b5d7e5fe94e5ee316d59b116
sd-cl <chicano8bit@gmail.com>
2023-03-18 02:13:46 -0300

    Calculadora completa

--- DOM.js
+++
@@ -1,6 +0,0 @@
-console.log(document.title);
-document.title= 'Prueba de titulo';
-console.log(document.body);
-var h1= document.getElementsByTagName('h1');
-h1[0].innerHTML= 'Prueba';
-console.log(h1);
