e78014aac0bc6764851df694f713f020179891c3
sd-cl <chicano8bit@gmail.com>
2023-03-18 00:09:17 -0300

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+var nombre = 'Simón'
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad = 34;
+console.log(edad);
+console.log(typeof(edad));
+
+edad = 'cinco';
+console.log(edad);
+console.log(typeof(edad));
+
+
+var sueldo = 1900.99;
+console.log(sueldo); 
+console.log(typeof(sueldo));
+
+var tienetrabajo = false;
+console.log(tienetrabajo); 
+console.log(typeof(tienetrabajo));
+
+
+var puestodetrabajo;
+console.log(puestodetrabajo);
+
+puestodetrabajo = null;
+console.log(puestodetrabajo);
+*/
+
+//OPERADORES LÓGICOS, UNARIOS Y DE ASIGNACIÓN
